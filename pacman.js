cfb218d8e196411cda4ebd6ba829f5caf098c91e
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-01-17 23:21:54 +0530

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,12 +1,3 @@
-/*jslint browser: true, undef: true, eqeqeq: true, nomen: true, white: true */
-/*global window: false, document: false */
-
-/*
- * fix looped audio
- * add fruits + levels
- * fix what happens when a ghost is eaten (should go back to base)
- * do proper ghost mechanics (blinky/wimpy etc)
- */
 
 var NONE        = 4,
     UP          = 3,
